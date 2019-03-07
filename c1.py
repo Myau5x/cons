06ee08dfc2bc1de3ea52577e2ea42caf14751336
Olga Illarionova <ola.illari@gmail.com>
2019-03-07 12:41:17 -0800

    add arg from command line

--- c1.py
+++ c1.py
@@ -6,10 +6,11 @@ Try to parse info about counsil session
 
 import requests
 from bs4 import BeautifulSoup
+import sys
 
 URL = 'https://washington.mid.ru/ru/consular-services/consulate/consul_sessions/'
 
-def check_sd(URL, pattern):
+def check_sd(url_cons, pattern):
     """
     Check that page with URL is the same as pattern
     """
@@ -17,7 +18,15 @@ def check_sd(URL, pattern):
     html = responce.content
     soup = BeautifulSoup(html,'html.parser')
     url_page = soup.select_one('div.page')
-    with open('pattern','r') as f:
+    with open(pattern,'r') as f:
         html2 = f.read()
         pat_page = BeautifulSoup(html2,'html.parser').select_one('div.page')
-    return url_page == pat_page
+    return url_page != pat_page
+
+if __name__ == "__main__":
+    pat  = sys.argv[1]
+    change  = check_sd(URL,pat)
+    if  change:
+        print('Panic!!!! Consul change something!')
+    else:
+        print('all same')
